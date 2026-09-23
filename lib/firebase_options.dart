50406399030fb013685390951ce02d6ea27a4695
asifkp <asifmuhammed195@gmail.com>
2026-09-23 17:53:52 +0530

    new changes

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,13 +1,23 @@
 // File generated for Firebase config.
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 class DefaultFirebaseOptions {
-  static FirebaseOptions? get currentPlatform {
+  static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
       return web;
     }
-    return null; // Let native configuration handle Android/iOS
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
   }
 
   static const FirebaseOptions web = FirebaseOptions(
@@ -19,5 +29,24 @@ class DefaultFirebaseOptions {
     messagingSenderId: "922708808396",
     appId: "1:922708808396:web:8e888703f7b49fd9716869",
     measurementId: "G-K1T51GCCR7",
+  );
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: "AIzaSyBfPup4nYLkejFDe9vUueRpVlQGy1sVj_g",
+    appId: "1:922708808396:android:4c4f46130e05fc8d716869",
+    messagingSenderId: "922708808396",
+    projectId: "kgra-ba502",
+    databaseURL: "https://kgra-ba502-default-rtdb.firebaseio.com",
+    storageBucket: "kgra-ba502.firebasestorage.app",
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: "AIzaSyBSYuyIwpsFAJmYRiPhmgIdfHAKJ981A0g",
+    appId: "1:922708808396:ios:08be48ff8ba28eb8716869",
+    messagingSenderId: "922708808396",
+    projectId: "kgra-ba502",
+    databaseURL: "https://kgra-ba502-default-rtdb.firebaseio.com",
+    storageBucket: "kgra-ba502.firebasestorage.app",
+    iosBundleId: "com.vexora.kgra",
   );
 }
